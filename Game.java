86e706ce76f890e81549e51be563b2634528589c
NathalieGreen <n.green@lancaster.ac.uk>
2018-03-21 22:30:42 +0000

    HIGHSCORES
    I have now created an if statement so that if all the icons are in the correct position (the player has won), it creates a new JFrame called high scores which I will devlop upon.

--- Game.java
+++ Game.java
@@ -28,24 +28,25 @@ public class Game implements ActionListener
         JPanel panel = new JPanel(); 
         JPanel panel2 = new JPanel();
         JLabel label = new JLabel("Your Score:" +currentscore);
+        JButton shuffle = new JButton("Shuffle");
+        
     public Game()
     {
         iconArray[0] = new ImageIcon("blank.jpg");
         for (int i = 1; i < buttonsArray.length; i++) {
             iconArray[i] = new ImageIcon("bart" + i + ".jpg");
         }
-		
-        
        
         panel.setPreferredSize( new Dimension(450, 380));
         GridLayout layout = new GridLayout(3,4);
         panel.setLayout(layout);
         main.add(panel);
       
-        panel2.setPreferredSize( new Dimension(450, 50));
+        panel2.setPreferredSize( new Dimension(450, 60));
         panel2.add(label);
+        shuffle.addActionListener(this);
         main.add(panel2);
-        
+        panel2.add(shuffle);
 
 
         for (int i = 0; i < buttonsArray.length; i++) {
@@ -57,18 +58,25 @@ public class Game implements ActionListener
         G.setContentPane(main);
       
         G.setTitle("Bart Puzzle");
-        G.setSize(450,430);
-        G.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        G.setSize(450,450);
+        G.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   //terminates application when closed
         G.setVisible(true);
+
     }
+     
 
     @Override
     public void actionPerformed(ActionEvent e) {
             checkMove(e.getSource());
             label.setText("Your Score:" +currentscore);
     }
-
+    
     public void checkMove(Object button){
+			  
+		  if(button == shuffle) {		
+			  System.out.println("you have pressed the shuffle button");
+			}
+		
         if(button == buttonsArray[0]) {
             if (buttonsArray[1].getIcon().toString().equals("blank.jpg")) {
                 buttonsArray[1].setIcon(buttonsArray[0].getIcon());
@@ -264,6 +272,48 @@ public class Game implements ActionListener
                 currentscore++;
             }
         }
+
+        if (buttonsArray[0].getIcon().toString().equals("blank.jpg") &&
+                buttonsArray[1].getIcon().toString().equals("bart1.jpg") &&
+                buttonsArray[2].getIcon().toString().equals("bart2.jpg") &&
+                buttonsArray[3].getIcon().toString().equals("bart3.jpg") &&
+                buttonsArray[4].getIcon().toString().equals("bart4.jpg") &&
+                buttonsArray[5].getIcon().toString().equals("bart5.jpg") &&
+                buttonsArray[6].getIcon().toString().equals("bart6.jpg") &&
+                buttonsArray[7].getIcon().toString().equals("bart7.jpg") &&
+                buttonsArray[8].getIcon().toString().equals("bart8.jpg") &&
+                buttonsArray[9].getIcon().toString().equals("bart9.jpg") &&
+                buttonsArray[10].getIcon().toString().equals("bart10.jpg") &&
+                buttonsArray[11].getIcon().toString().equals("bart11.jpg")) {
+                 System.out.println("You Win!!!");
+                 String yourname;
+                 yourname =JOptionPane.showInputDialog("WELL DONE, YOU WIN!! What is your name?");
+                 System.out.println(yourname + currentscore);
+
+                 JFrame h = new JFrame ();  //creates a new blank window
+                 h.setTitle("High Scores");     //changes window title
+                 h.setSize(300,300);        //changes window size
+                 h.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); //closes window but keeps app running
+                 h.setVisible(true);        //makes window visible
+
+                //GridLayout layout2 = new GridLayout(10,2);
+                //JPanel panel3 = new JPanel();
+                //panel3.setLayout(layout2);
+
+
+                JLabel name = new JLabel("name", JLabel.LEFT);
+                JLabel score = new JLabel("score", JLabel.RIGHT);
+
+                h.add(name);
+                h.add(score);
+
+                //panel3.add(name);
+                //panel3.add(score);
+
+
+
+        }
        
     }
+   
 }
